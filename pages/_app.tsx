d612383bf8971381d7d584b2acab1a8999fcc476
Alex Yermolaev <ayermolaev@pm.me>
2021-12-24 14:47:58 -0500

    common component.

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import {AppProps} from "next/app"
+
+function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <div className="app-component">
+      <Component {...pageProps} />
+    </div>
+  )
+}
+
+export default MyApp

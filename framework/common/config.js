858cf014b62444e0d579f953001ee4996b1c2c28
Alex Yermolaev <ayermolaev@pm.me>
2021-12-24 01:42:39 -0500

    framework configs

---
+++ framework/common/config.js
@@ -0,0 +1,11 @@
+const path = require("path")
+const merge = require("deepmerge")
+
+function withFrameworkConfig(defaultConfig = {}) {
+    const framework = "shopify"
+    const frameworkNextConfig =
+        require(path.join("../", framework, "next.config"))
+    return merge(defaultConfig, frameworkNextConfig)
+}
+
+module.exports = { withFrameworkConfig }
--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
+const { withFrameworkConfig } = require("./framework/common/config")
+
+module.exports = withFrameworkConfig({
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ["en-US", "es"],
+    defaultLocale: "en-US"
+  }
+})
